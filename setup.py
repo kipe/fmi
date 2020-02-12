c567622ad29a14a03645b8bc8c7565f315b53480
Kimmo Huoman <kipenroskaposti@gmail.com>
2020-02-12 19:02:59 +0200

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='fmi_weather',
-    version='0.80.0',
+    version='1.0.0',
     description='FMI weather data fetcher',
     author='Kimmo Huoman',
     author_email='kipenroskaposti@gmail.com',
